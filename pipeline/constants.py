bc1b7ad2e53668e98802d019e31adca8cae0be25
Lining Wang <tinydeltas@gmail.com>
2021-10-31 13:12:39 -0400

    refactor and clean up code

---
+++ pipeline/constants.py
@@ -0,0 +1,19 @@
+input_image_types = [ 
+    "mel", 
+    "chroma", 
+    "mfcc", 
+    "sfft", 
+    "sfftchroma",
+    "spectrogram" 
+]
+
+gestures = [
+    "close", 
+    "cut", 
+    "no", 
+    "oldown", # aka voldown 
+    "olup", # aka volup
+    "open", 
+    "switch", 
+    "tap"
+]
