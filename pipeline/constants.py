8c9fea215db2260c41a5a49af7921faaf9d6d8f8
Lining Wang <lining.somewhere@gmail.com>
2021-11-03 14:20:33 -0400

    start on docs

--- pipeline/constants.py
+++ pipeline/constants.py
@@ -1,12 +1,17 @@
+# The input image (spectrogram) types we are interested in feeding 
+# to the ML model for training and validation. 
 input_image_types = [ 
-    "mel", 
-    "chroma", 
-    "mfcc", 
-    "sfft", 
-    "sfftchroma",
-    "spectrogram" 
+    "mel",              # Mel spectrogram
+    "chroma",           # Chromagram
+    "stftchroma",       # Chromagram with short-time fourier transform applied
+    "mfcc",             # Mel cepstral coefficient transform 
+    "stft",             # Short-time fourier transform 
+    "spectrogram"       # Generic spectrogram
 ]
 
+# Labels for the pre-defined gestures we are interested in testing 
+# performance for. Corresponds to the names of their respective folders 
+# in `data/images`
 gestures = [
     "close", 
     "cut", 
